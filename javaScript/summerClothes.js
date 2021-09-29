2aec9dab36b4c1d7cc581cf1225f82d6c100a0b6
Vikas Kumar <vsrathod39@gmail.com>
2021-09-29 15:32:37 +0530

    clothes_page_in_progress

--- javaScript/summerClothes.js
+++ javaScript/summerClothes.js
@@ -2,7 +2,7 @@ let productData = [
     {
         image: "https://m.media-amazon.com/images/I/71OhHJi+72L.jpg",
         name: "Men's 2 Piece Tracksuit,Men’s Summer 2-Piece Beach Cotton Linen Short Sleeve Shirts & Shorts Pants Sets",
-        brand: "Kouzhaoa",
+        brand: "kouzhaoa",
         rating: "3.9",
         price: "4919",
         origin: "USA"
@@ -10,7 +10,7 @@ let productData = [
     {
         image: "https://m.media-amazon.com/images/I/5111U5C9NVL.jpg",
         name: "Men's Casual Long Pants Linen Pants - Loose Lightweight Casual Trousers Summer Yoga Beach Trousers",
-        brand: "Percle",
+        brand: "percle",
         rating: "3.6",
         price: "4609",
         origin: "USA"
@@ -18,7 +18,7 @@ let productData = [
     {
         image: "https://m.media-amazon.com/images/I/91vNaFnBJES.jpg",
         name: "EUOW Men's Hawaiian Shirt Short Sleeves Printed Button Down Summer Beach Dress Shirts",
-        brand: "Euow",
+        brand: "euow",
         rating: "4.5",
         price: "4729",
         origin: "USA"
@@ -26,11 +26,75 @@ let productData = [
     {
         image: "https://m.media-amazon.com/images/I/61yrclbiu2S.jpg",
         name: "Vintage Shirts for Men Striped Short Sleeve Hawaiian Shirt Summer Button Down T Shirt Relaxed-Fit Casual Beach Tops",
-        brand: "Zhuer",
+        brand: "zhuer",
         rating: "3.2",
         price: "2589",
         origin: "USA"
-    }
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/71xxyK6dqQS.jpg",
+        name: "Summer Casual Tshirt Dresses for Women Swing Sun Dress Beach Swimsuit Cover Ups with Pockets",
+        brand: "moskill",
+        rating: "4.2",
+        price: "4809",
+        origin: "USA"
+    },
+    {
+        image: "https://images-na.ssl-images-amazon.com/images/I/61fTwyrYMqL.SS150.jpg",
+        name: "Womens Summer Midi Dress Striped Short Sleeve Dresses Side Split Casual Waist Gowns",
+        brand: "vonda",
+        rating: "3.4",
+        price: "1349",
+        origin: "UAE"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/615oUasaNKL.jpg",
+        name: "Blooming Jelly Women's Halter Neck Deep V Asymmetrical Floral Dress Lace Summer Maxi Dress",
+        brand: "Blooming Jelly",
+        rating: "4",
+        price: "6189",
+        origin: "UK"
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/51ZXzbnw9lL.jpg",
+        name: "Women's Pleated Dress (White)",
+        brand: "Norah",
+        rating: "2.8",
+        price: "2159",
+        origin: "India"
+    },
+    {
+        image: "https://images-na.ssl-images-amazon.com/images/I/71suU%2BE1K2S.SS150.jpg",
+        name: "Women's Polyester Western Dress",
+        brand: "Meloso",
+        rating: "3.8",
+        price: "1289",
+        origin: "India"
+    },
+    {
+        image: "",
+        name: "",
+        brand: "",
+        rating: "",
+        price: "",
+        origin: ""
+    },
+    {
+        image: "",
+        name: "",
+        brand: "",
+        rating: "",
+        price: "",
+        origin: ""
+    },
+    {
+        image: "",
+        name: "",
+        brand: "",
+        rating: "",
+        price: "",
+        origin: ""
+    },
 ];
 
 // function used to showProducts Data from an array named productData to html clothes page
@@ -45,30 +109,44 @@ function showProducts(product){
         let productImg =  document.createElement("img");
         productImg.src = element.image;
 
+        let textBox = document.createElement("div");
+        textBox.setAttribute("class", "textBox");
+
         let productName = document.createElement("p");
         productName.textContent = element.name;
 
         let productBrand = document.createElement("p");
-        productBrand.textContent = element.brand;
+        productBrand.classList.add("textGreyColor");
+        productBrand.textContent = "By " + element.brand;
+        textBox.append(productName);
 
         let productWrapper = document.createElement("div");
         productWrapper.setAttribute("class", "productWrapper");
 
         let wrapperInnerDiv = document.createElement("div");
         let productRating = document.createElement("p");
-        productRating.textContent = element.rating;
+        productRating.textContent = "★ " + element.rating;
 
         let productPrice = document.createElement("p");
-        productPrice.textContent = element.price;
+        productPrice.setAttribute("id", "textPrice");
+        productPrice.textContent = "₹ " + element.price[0] +  "," + element.price.substring(1, element.price.length);
         wrapperInnerDiv.append(productRating, productPrice);
 
         let wrapperInnerDiv2 = document.createElement("div");
-        let productOrigin = document.createElement("p");
-        productOrigin.textContent = "from " + element.origin + " to INDIA";
-        wrapperInnerDiv2.append(productOrigin);
+        wrapperInnerDiv2.classList.add("origin", "textGreyColor");
 
-        productWrapper.append(productImg, productName, productBrand, wrapperInnerDiv, wrapperInnerDiv2);
-        productBox.append(productWrapper);
+        let productOrigin = document.createElement("p");
+        productOrigin.textContent = "from " + element.origin;
+
+        let productShipping = document.createElement("p");
+        productShipping.textContent = "to INDIA";
+
+        let productShipDate = document.createElement("p");
+        productShipDate.textContent = "in 9-15 days"
+        wrapperInnerDiv2.append(productOrigin, productShipping, productShipDate);
+
+        productWrapper.append(wrapperInnerDiv, wrapperInnerDiv2);
+        productBox.append(productImg, textBox, productBrand, productWrapper);
         globalProducts.append(productBox);
     });
 }
