c81fc6d48d5530ea19227639fa33b9957ade6c0a
Vikas Kumar <vsrathod39@gmail.com>
2021-09-29 16:42:08 +0530

    clothes_page_completed

--- javaScript/summerClothes.js
+++ javaScript/summerClothes.js
@@ -32,6 +32,97 @@ let productData = [
         origin: "USA"
     },
     {
+        image: "https://m.media-amazon.com/images/I/81v6P5f0+aL.jpg",
+        name:"YOMOVER Mens Shorts Basketball Pack 9 inch Athletic Workout Gym Running Home Quick Dry Shorts for Men with Pockets",
+        brand: "Yomover",
+        rating: "4.4",
+        price: "6229",
+        origin: "USA",
+    },
+    {
+        image: "https://m.media-amazon.com/images/I/61ilzGwJgOL.jpg",
+        name:"Men UA Tech 2.0 SS Tee Novelty, Sports T-Shirt, Gym Clothes",
+        brand: "Under Armour",
+        rating: "4.6",
+        price: "5689",
+        origin: "UK",
+    },
+    // {
+    //     image: "https://m.media-amazon.com/images/I/71xxyK6dqQS.jpg",
+    //     name: "Summer Casual Tshirt Dresses for Women Swing Sun Dress Beach Swimsuit Cover Ups with Pockets",
+    //     brand: "moskill",
+    //     rating: "4.2",
+    //     price: "4809",
+    //     origin: "USA"
+    // },
+    // {
+    //     image: "https://images-na.ssl-images-amazon.com/images/I/61fTwyrYMqL.SS150.jpg",
+    //     name: "Womens Summer Midi Dress Striped Short Sleeve Dresses Side Split Casual Waist Gowns",
+    //     brand: "vonda",
+    //     rating: "3.4",
+    //     price: "1349",
+    //     origin: "UAE"
+    // },
+    // {
+    //     image: "https://m.media-amazon.com/images/I/615oUasaNKL.jpg",
+    //     name: "Blooming Jelly Women's Halter Neck Deep V Asymmetrical Floral Dress Lace Summer Maxi Dress",
+    //     brand: "Blooming Jelly",
+    //     rating: "4",
+    //     price: "6189",
+    //     origin: "UK"
+    // },
+    // {
+    //     image: "https://m.media-amazon.com/images/I/51ZXzbnw9lL.jpg",
+    //     name: "Women's Pleated Dress (White)",
+    //     brand: "Norah",
+    //     rating: "2.8",
+    //     price: "2159",
+    //     origin: "India"
+    // },
+    // {
+    //     image: "https://images-na.ssl-images-amazon.com/images/I/71suU%2BE1K2S.SS150.jpg",
+    //     name: "Women's Polyester Western Dress",
+    //     brand: "Meloso",
+    //     rating: "3.8",
+    //     price: "1289",
+    //     origin: "India"
+    // },
+    // {
+    //     image: "https://m.media-amazon.com/images/I/71yNQWMEG4S.jpg",
+    //     name: "KILIG Women's Sleeveless/Long Sleeve Dresses Casual Button Down Summer Dress with Pockets",
+    //     brand: "kilig",
+    //     rating: "4.1",
+    //     price: "19779",
+    //     origin: "UK"
+    // },
+    // {
+    //     image: "https://m.media-amazon.com/images/I/71E2X80sLML.jpg",
+    //     name: "Roman Originals Women Contrast Print Hanky Hem Dress - Ladies Summer Holiday Beach Casual Everyday Comfort Garden Party BBQ Sleeveless Asymmetric Pull On Jersey Dress",
+    //     brand: "Roman Originals",
+    //     rating: "4.7",
+    //     price: "9749",
+    //     origin: "UK"
+    // },
+    // {
+    //     image: "https://m.media-amazon.com/images/I/71C3Ox89u3L.jpg",
+    //     name: "Women's Summer Wrap V Neck Polka Dot Print Ruffle Short Sleeve Mini Floral Dress with Belt",
+    //     brand: "Naggoo",
+    //     rating: "4.1",
+    //     price: "4539",
+    //     origin: "USA"
+    // },
+    // {
+    //     image: "https://m.media-amazon.com/images/I/71hwiN0YWLL.jpg",
+    //     name:"MEROKEETY Women's Summer Striped Short Sleeve T Shirt Dress Casual Tie Waist Midi Dress",
+    //     brand: "Merokeety",
+    //     rating: "4.4",
+    //     price: "8859",
+    //     origin: "USA",
+    // }
+];
+
+let productData2 = [
+    {
         image: "https://m.media-amazon.com/images/I/71xxyK6dqQS.jpg",
         name: "Summer Casual Tshirt Dresses for Women Swing Sun Dress Beach Swimsuit Cover Ups with Pockets",
         brand: "moskill",
@@ -72,35 +163,44 @@ let productData = [
         origin: "India"
     },
     {
-        image: "",
-        name: "",
-        brand: "",
-        rating: "",
-        price: "",
-        origin: ""
+        image: "https://m.media-amazon.com/images/I/71yNQWMEG4S.jpg",
+        name: "KILIG Women's Sleeveless/Long Sleeve Dresses Casual Button Down Summer Dress with Pockets",
+        brand: "kilig",
+        rating: "4.1",
+        price: "19779",
+        origin: "UK"
     },
     {
-        image: "",
-        name: "",
-        brand: "",
-        rating: "",
-        price: "",
-        origin: ""
+        image: "https://m.media-amazon.com/images/I/71E2X80sLML.jpg",
+        name: "Roman Originals Women Contrast Print Hanky Hem Dress - Ladies Summer Holiday Beach Casual Everyday Comfort Garden Party BBQ Sleeveless Asymmetric Pull On Jersey Dress",
+        brand: "Roman Originals",
+        rating: "4.7",
+        price: "9749",
+        origin: "UK"
     },
     {
-        image: "",
-        name: "",
-        brand: "",
-        rating: "",
-        price: "",
-        origin: ""
+        image: "https://m.media-amazon.com/images/I/71C3Ox89u3L.jpg",
+        name: "Women's Summer Wrap V Neck Polka Dot Print Ruffle Short Sleeve Mini Floral Dress with Belt",
+        brand: "Naggoo",
+        rating: "4.1",
+        price: "4539",
+        origin: "USA"
     },
-];
+    {
+        image: "https://m.media-amazon.com/images/I/71hwiN0YWLL.jpg",
+        name:"MEROKEETY Women's Summer Striped Short Sleeve T Shirt Dress Casual Tie Waist Midi Dress",
+        brand: "Merokeety",
+        rating: "4.4",
+        price: "8859",
+        origin: "USA",
+    }
+]
 
+let menContainer = document.getElementById("menContainer");
+let womenContainer = document.getElementById("womenContainer");
 // function used to showProducts Data from an array named productData to html clothes page
-function showProducts(product){
-    let globalProducts = document.getElementById("globalProducts");
-    globalProducts.innerHTML = null;
+function showProducts(product, containerId){
+    containerId.innerHTML = null;
 
     product.forEach(function (element) {
         let productBox = document.createElement("div");
@@ -147,8 +247,10 @@ function showProducts(product){
 
         productWrapper.append(wrapperInnerDiv, wrapperInnerDiv2);
         productBox.append(productImg, textBox, productBrand, productWrapper);
-        globalProducts.append(productBox);
+        containerId.append(productBox);
     });
 }
 
-showProducts(productData);
+showProducts(productData, menContainer);
+showProducts(productData2, womenContainer);
+
