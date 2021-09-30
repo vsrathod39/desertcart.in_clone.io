88bb83d65ddeb2b6ebf8ce95cf0a323cdf785e41
Vikas Kumar <vsrathod39@gmail.com>
2021-09-30 18:59:22 +0530

    product_add_to_cart_page

--- javaScript/productReview.js
+++ javaScript/productReview.js
@@ -3,58 +3,82 @@ let currentPro = [
         image: "https://m.media-amazon.com/images/I/71g+JfQTkqL.jpg",
         name: "Womens Fall Clothes Stitching Geo Stripe Print Coat Oversized Loose Pocket Top Lapel Long-Sleeve Button Tunic",
         brand: "Afelkas",
-        rating: "",
+        rating: "4",
         price: "4679",
         origin: "USA"
     }
 ];
 
-let proImage = document.getElementById("proImage");
-let proSubImage = document.getElementById("proSubImage");
-let proAddToCart = document.getElementById("proAddToCart");
 
+// let productLogo = document.getElementById("productLogo");
 
-function appendImg(){
+function showProductImage(){
+    let productImage = document.getElementById("productImage");
+
     let img = document.createElement("img");
     img.src = currentPro[0].image;
-    proImage.append(img);
-    proAddToCart.append(name);
-}
-function proSubIcon(){
-    let fav = document.createElement("img");
-    fav.src = "https://img.icons8.com/material-outlined/24/000000/like--v1.png";
 
-    let share = document.createElement("img");
-    share.src = "https://img.icons8.com/material/24/000000/share--v1.png"
-
-    let smallImg = document.createElement("img");
-    smallImg.src = currentPro[0].image;
-    smallImg.setAttribute("id", "imgIcon");
-
-    proSubImage.append(fav, share, smallImg);
+    productLogo.append(img);
+    productImage.append(img);
 }
 
-function cartDetails(){
+function showProductLogo(){
+    let productLogoImage = document.getElementById("productLogoImage");
+    let img = document.createElement("img");
+    img.src = currentPro[0].image;
+    productLogoImage.append(img);
+}
+
+function showPriceRating(){
+    let priceId = document.getElementById("price");
+    let ratingId = document.getElementById("rating");
+
+    let brand = document.createElement("p");
+    brand.setAttribute("clsss", "textGreyColor")
+    brand.textContent = "Brand : " + currentPro[0].brand;
+
+    let price = document.createElement("p");
+    price.setAttribute("class", "boldFont")
+    price.textContent = "₹ " + currentPro[0].price[0] + "," + currentPro[0].price.substring(1,  currentPro[0].price.length);
+
+    let rating = document.createElement("p");
+    rating.setAttribute("class", "origin")
+    rating.textContent = "★ " + currentPro[0].rating;
+
+    let wrapperInnerDiv2 = document.createElement("div");
+    wrapperInnerDiv2.classList.add("origin");
+
+    let productOrigin = document.createElement("p");
+    productOrigin.textContent = "from " +  currentPro[0].origin;
+
+    let productShipping = document.createElement("p");
+    productShipping.textContent = "to INDIA";
+
+    let productShipDate = document.createElement("p");
+    productShipDate.textContent = "in 9-15 days"
+    wrapperInnerDiv2.append(productOrigin, productShipping, productShipDate);
+    // let delivery = document.createElement("p");
+    // delivery.textContent = currentPro[0].image;
+
+    priceId.append(brand, price);
+    ratingId.append(rating, wrapperInnerDiv2);
+
+}
+
+function showProductname(){
     let productName = document.getElementById("productName");
-    let h1 = document.createElement("h1");
-    h1.textContent = currentPro[0].name;
-    h1.setAttribute("id", "proName")
 
-    productName.append(h1);
+    let headding = document.createElement("h1");
+    headding.textContent = currentPro[0].name;
+
+    productName.append(headding);
 }
+
 function productReview(){
-    appendImg();
-    proSubIcon();
-    cartDetails();
-
-    // let chartBox = document.createElement("div");
-    // chartBox.setAttribute("id", "chartBox")
-
-    // let sectionHeading = document.createElement("p");
-    // sectionHeading.textContent = "Size";
-
-    // chartBox.append(sectionHeading);
-    // proAddToCart.append(chartBox);
+    showProductImage();
+    showProductLogo();
+    showPriceRating();
+    showProductname();
 }
 
-productReview();
+productReview()
