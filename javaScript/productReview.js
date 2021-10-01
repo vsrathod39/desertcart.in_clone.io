21b842093bf75e886c54f3012067378c5bb728bf
Vikas Kumar <vsrathod39@gmail.com>
2021-10-01 12:37:26 +0530

    review

--- javaScript/productReview.js
+++ javaScript/productReview.js
@@ -57,12 +57,14 @@ function showPriceRating(){
     let productShipDate = document.createElement("p");
     productShipDate.textContent = "in 9-15 days"
     wrapperInnerDiv2.append(productOrigin, productShipping, productShipDate);
-    // let delivery = document.createElement("p");
-    // delivery.textContent = currentPro[0].image;
 
     priceId.append(brand, price);
     ratingId.append(rating, wrapperInnerDiv2);
 
+    let btn = document.getElementById("cartButton");
+    let btnCart = document.createElement("button");
+    btnCart.textContent = "Add to Cart " + "₹ " + currentPro[0].price[0] + "," + currentPro[0].price.substring(1,  currentPro[0].price.length);
+    btn.append(btnCart);
 }
 
 function showProductname(){
@@ -74,11 +76,74 @@ function showProductname(){
     productName.append(headding);
 }
 
+let descriptionData = ["Pull On closure.", "Are Rinse by hand in warm water max 30 °, do not bleach. Wash before use, do not iron, do not dry clean..", "mens sweatsuits set,sweatsuits,shirt,suit men,mens shits,man clothes,mens jogger suit sets,men fashions,men sets,tracksuit men set,mens fashion,track suit men,mens pants fashions men,men clothing 2021,men two piece set.", "clothing mens sets of clothing,men set,men track suits 2021,tracksuit,mens tracksuit,mens sports suits,pants men,men outfits,men outfit set, sweatsuits men,men short sets,men sweatsuit,romper,tracksuits shirts.", "High Waist, Cheap Tracksuits, Lightweight, Casual Two Piece Outfit Set, Men Tracksuit Set, Mens Summer Clothes, Casual Two Piece Set 2 piece tracksuit mens tracksuits mens outfit tracksuit gym clothing men fashion mens romper."];
+
+function showDescription(){
+    let descriptionBox = document.getElementById("descriptionBox");
+    let ul = document.createElement("ul");
+
+    descriptionData.forEach(function(data){
+        let li =document.createElement("li");
+        let disc = document.createElement("p");
+        disc.textContent = data;
+
+        li.append(disc);
+        ul.append(li);
+    });
+    descriptionBox.append(ul);
+}
+
+let reviewData = [
+    {
+        name: "M***K",
+        subject: "Would order every color! Lite weight comfortable! Order 2 sizes up, they fit small.",
+        rating: "⭐⭐⭐⭐⭐",
+        revv: "I'm a large and ordered a XL and was still a bit to small,I ordered 3 more in different colors 2XL and fits perfectly."
+    },
+    {
+        name: "E***Y",
+        subject: "does not fit",
+        rating: "⭐⭐☆☆☆",
+        revv: "product was 2 sizes smaller than the lable"
+    },
+    {
+        name: "M***K",
+        subject: "Orange one looks like a prison uniform",
+        rating: "⭐⭐☆☆☆",
+        revv: "The material is kind of crappy and the orange looks exactly like prison orange. Paired with the cheap craftsmanship"
+    }
+];
+
+function showReview(){
+    let reviewBox = document.getElementById("reviewBox");
+    reviewData.forEach(function(rev){
+        let div = document.createElement("div");
+
+        let name  = document.createElement("p");
+        name.textContent = rev.name;
+
+        let subject = document.createElement("p");
+        subject.setAttribute("id", "boldSub");
+        subject.textContent = rev.subject;
+
+        let rating = document.createElement("p");
+        rating.textContent = rev.rating;
+
+        let revv = document.createElement("p");
+        revv.textContent = rev.revv;
+
+        div.append(name, subject, rating, revv);
+        reviewBox.append(div);
+    });
+}
+
 function productReview(){
     showProductImage();
     showProductLogo();
     showPriceRating();
     showProductname();
+    showDescription();;
+    showReview();
 }
 
 productReview()
