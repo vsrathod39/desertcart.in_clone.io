b876d7487d72bdeec020c191afe686317e9839e8
Vikas Kumar <vsrathod39@gmail.com>
2021-10-27 14:16:19 +0530

    Revert "revert"
    This reverts commit 86058f0d4f34bd9c9c46954df388371bc34495af.

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,8 +1,5 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 =======
-=======
->>>>>>> parent of 60d0933 (local_Storage_issues_fixed)
 if(localStorage.getItem() !== null){
     let cartPro = JSON.parse(localStorage.getItem("disertCartItem"));
     console.log(cartPro);
